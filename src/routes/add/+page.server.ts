32913cdb4f9303ac31a360c15d06c5a6f8654dbe
Sanyam Jain <xerosanyam@gmail.com>
2024-03-22 17:23:29 +0530

    add card form

---
+++ src/routes/add/+page.server.ts
@@ -0,0 +1,31 @@
+import type { RequestEvent } from "./$types.js";
+import { fail, redirect } from "@sveltejs/kit";
+import { superValidate } from "sveltekit-superforms";
+import { zod } from "sveltekit-superforms/adapters";
+import { z } from "zod";
+import { sleep } from "$lib/common.util.js";
+
+const schema = z.object({
+	front: z.string().min(1).max(140),
+	back: z.string().min(1).max(140)
+});
+
+export async function load() {
+	const form = await superValidate(zod(schema));
+
+	return { form };
+}
+
+export const actions = {
+	default: add
+}
+
+async function add(event: RequestEvent) {
+	await sleep(1000)
+	const form = await superValidate(event, zod(schema));
+	if (!form.valid) {
+		return fail(400, { form });
+	}
+	redirect(302, '/add')
+}
+
