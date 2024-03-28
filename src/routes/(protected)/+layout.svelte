b2b94afa03e6df0a7889bc74c9846d7113aefe3e
Sanyam Jain <xerosanyam@gmail.com>
2024-03-29 00:27:20 +0530

    feat(layout->protected): add layout for protected routes, add logout button

---
+++ src/routes/(protected)/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Logout from '$lib/components/Logout.svelte';
+	import { ROUTES } from '$lib/routes.util';
+</script>
+
+<nav>
+	<a href={ROUTES.HOME}>Home</a>
+	<Logout />
+</nav>
+<slot />
