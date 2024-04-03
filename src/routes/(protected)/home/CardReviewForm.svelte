817c7b37054deaa31e08c545ed70eaa4a4fc4949
Sanyam Jain <xerosanyam@gmail.com>
2024-04-03 22:54:05 +0530

    refactor(cardReviewForm): remove <SuperDebug/>

--- src/routes/(protected)/home/CardReviewForm.svelte
+++ src/routes/(protected)/home/CardReviewForm.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
 	import { difficulty, type CardReviewSchema } from '$lib/schemas';
-	import SuperDebug, { superForm, type Infer, type SuperValidated } from 'sveltekit-superforms';
+	import { superForm, type Infer, type SuperValidated } from 'sveltekit-superforms';
 
 	export let data: SuperValidated<Infer<CardReviewSchema>>;
-	const { form, errors, constraints } = superForm(data);
+	const { errors } = superForm(data);
 </script>
-
-<SuperDebug data={{ $form, $errors, $constraints }} />
 
 <h1>Try to recall the answer</h1>
 <form method="post" action="?/review">
