53c776e293f7ae49136c0b66a6c68d4bc515a87f
Sanyam Jain <xerosanyam@gmail.com>
2024-04-13 03:34:14 +0530

    update variable name to improve security

--- src/lib/db/card.util.ts
+++ src/lib/db/card.util.ts
@@ -1,4 +1,5 @@
-import { sql } from '$lib/db/db.util';
+import { authTxn } from '$lib/db/db.util';
+import type postgres from 'postgres';
 
 export const insertCard = async ({
 	id,
@@ -12,8 +13,9 @@ export const insertCard = async ({
 	user_id: string;
 }) => {
 	try {
-		await sql`INSERT INTO remfo.card(id, front, back, user_id)
+		const query = (sql: postgres.TransactionSql) => sql`INSERT INTO remfo.card(id, front, back, user_id)
   				VALUES (${id}, ${front}, ${back}, ${user_id})`;
+		await authTxn(user_id, query)
 	} catch (err) {
 		console.error('insertCard ~ err:', err)
 	}
--- src/lib/db/db.util.ts
+++ src/lib/db/db.util.ts
@@ -1,7 +1,8 @@
 import { CONNECTION_STRING } from '$env/static/private';
 import postgres from 'postgres';
 
-export const sql = postgres(CONNECTION_STRING, {
+// be cautious while using sql_postgres. it bypasses RLS.
+export const sql_postgres = postgres(CONNECTION_STRING, {
 	prepare: false,
 	connection: {
 		search_path: 'remfo',
@@ -10,7 +11,7 @@ export const sql = postgres(CONNECTION_STRING, {
 
 export const testConnection = async () => {
 	try {
-		const rows = await sql`SELECT NOW()`;
+		const rows = await sql_postgres`SELECT NOW()`;
 		console.info(rows[0]);
 		return true;
 	} catch (err) {
