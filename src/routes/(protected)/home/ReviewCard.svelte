87ed454be01c35fddabaaec4bfa4210d2c9307e6
Sanyam Jain <xerosanyam@gmail.com>
2024-05-29 06:38:03 -0700

    reorder buttons

--- src/routes/(protected)/home/ReviewCard.svelte
+++ src/routes/(protected)/home/ReviewCard.svelte
@@ -11,8 +11,8 @@
 
 	const options = {
 		Easy: 'Super easy',
-		Good: 'Yes',
-		Hard: 'No',
+		Good: 'Easy',
+		Hard: 'Difficult',
 		Challenging: 'Super difficult'
 	};
 
@@ -31,26 +31,45 @@
 				{card.front}
 			</div>
 			<div
-				class="flex max-h-96 w-full overflow-y-auto rounded-md border bg-background px-3 py-2 text-sm font-medium ring-offset-background blur-sm placeholder:text-muted-foreground hover:filter-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+				class="flex max-h-96 w-full overflow-y-auto rounded-md border bg-background text-sm font-medium ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
 			>
-				{card.back}
+				<div class="w-full px-3 py-2 blur-sm hover:filter-none focus:filter-none">{card.back}</div>
 			</div>
 			<form method="post" action="?/review" class="w-full">
 				<input type="hidden" name="cardId" value={card.id} />
 				<div class="w-full">
 					<div class="text-sm">3. Were you able to recall it easily ?</div>
-					<div class="mt-3 flex justify-center space-x-4">
-						<button
-							class="inline-flex w-1/2 items-center justify-center space-x-1 whitespace-nowrap rounded-md border border-dashed border-input bg-background px-4 py-2 text-base ring-offset-background transition-colors hover:border-solid hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-							name="difficulty"
-							value={'Good'}><CheckIcon /><span>{options['Good']}</span></button
-						>
-
-						<button
-							class="inline-flex w-1/2 items-center justify-center space-x-1 whitespace-nowrap rounded-md border border-dashed border-input bg-background px-4 py-2 text-base ring-offset-background transition-colors hover:border-solid hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-							name="difficulty"
-							value={'Hard'}><CloseIcon /><span>{options['Hard']}</span></button
-						>
+					<div class="mt-3 flex justify-center">
+						<div class="flex w-1/2 flex-col items-center space-y-2 border-r p-2">
+							<div class="flex items-center">
+								<CheckIcon class="mr-0.5" />
+							</div>
+							<button
+								class="inline-flex w-11/12 items-center justify-center space-x-1 whitespace-nowrap rounded-md border border-dashed border-input bg-background px-4 py-3 text-sm ring-offset-background transition-colors hover:border-solid hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+								name="difficulty"
+								value={'Good'}>{options['Good']}</button
+							>
+							<button
+								class="inline-flex w-11/12 items-center justify-center space-x-1 whitespace-nowrap rounded-md border border-dashed border-input bg-background px-4 py-1 text-sm ring-offset-background transition-colors hover:border-solid hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+								name="difficulty"
+								value={'Easy'}>{options['Easy']}</button
+							>
+						</div>
+						<div class="flex w-1/2 flex-col items-center space-y-2 p-2">
+							<div class="flex"><CloseIcon class="mr-1" /></div>
+							<button
+								class="inline-flex w-11/12 items-center justify-center space-x-1 whitespace-nowrap rounded-md border border-dashed border-input bg-background px-4 py-3 text-sm ring-offset-background transition-colors hover:border-solid hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+								name="difficulty"
+								value={'Hard'}>{options['Hard']}</button
+							>
+							<button
+								class="inline-flex w-11/12 items-center justify-center space-x-1 whitespace-nowrap rounded-md border border-dashed border-input bg-background px-4 py-1 text-sm ring-offset-background transition-colors hover:border-solid hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+								name="difficulty"
+								value={'Challenging'}
+							>
+								{options['Challenging']}
+							</button>
+						</div>
 					</div>
 				</div>
 			</form>
