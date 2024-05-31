469181861cfd48ec30087961f94da2c921f679cf
Sanyam Jain <xerosanyam@gmail.com>
2024-05-30 17:58:09 -0700

    submit form programatically

--- src/routes/(protected)/learn/GenerateFlashCard.svelte
+++ src/routes/(protected)/learn/GenerateFlashCard.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import { applyAction, enhance } from '$app/forms';
 	import type { CardLearnSchema } from '$lib/schemas';
+	import { shortcut } from '$lib/shortcuts';
 	import { superForm, type Infer, type SuperValidated } from 'sveltekit-superforms';
 
 	export let data: SuperValidated<Infer<CardLearnSchema>>;
 	const { form, errors, constraints } = superForm(data);
 	let loading = false;
+	let formRef: HTMLFormElement;
 </script>
 
 <form
@@ -18,6 +20,7 @@
 			await applyAction(result);
 		};
 	}}
+	bind:this={formRef}
 >
 	<textarea
 		class="flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
@@ -28,6 +31,11 @@
 		bind:value={$form.userInput}
 		{...$constraints.userInput}
 		data-gramm="false"
+		use:shortcut={{
+			control: true,
+			code: 'Enter',
+			callback: () => formRef.dispatchEvent(new Event('submit', { bubbles: true }))
+		}}
 	></textarea>
 	<div>
 		<button
