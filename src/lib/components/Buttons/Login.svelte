a86e86d4d60e2cbe621fc52557c80eb2a59b75d5
Sanyam Jain <xerosanyam@gmail.com>
2024-06-02 04:34:21 -0700

    update login

--- src/lib/components/Buttons/Login.svelte
+++ src/lib/components/Buttons/Login.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	import LoginIcon from '~icons/material-symbols-light/login-outline-rounded';
+	import SqrlLoginIcon from '~icons/arcticons/signaturespoofingchecker';
 	export let text: string = 'Sign In';
 </script>
 
 <form action="/login/google">
 	<button
-		class="flex items-center space-x-2 rounded-sm bg-gray-900 px-3 py-2 text-white"
+		class="flex w-full items-center space-x-2 px-4 py-4 text-gray-600 hover:bg-gray-100"
 		type="submit"
 	>
-		<LoginIcon style="font-size:1.2rem;" />
+		<SqrlLoginIcon style="font-size:1.2rem;" />
 		<span>{text} </span>
 	</button>
 </form>
