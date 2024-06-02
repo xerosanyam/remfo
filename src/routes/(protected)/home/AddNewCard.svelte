c5fd693e671a42bfa6a0b79c5f3a293292285782
Sanyam Jain <xerosanyam@gmail.com>
2024-06-02 04:39:27 -0700

    update add card

--- src/routes/(protected)/home/AddNewCard.svelte
+++ src/routes/(protected)/home/AddNewCard.svelte
@@ -41,7 +41,7 @@
 						for="question">Question</label
 					>
 					<textarea
-						class="flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+						class="flex min-h-[80px] w-full rounded-md border border-input px-3 py-2 ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
 						id="question"
 						name="front"
 						bind:value={$form.front}
@@ -57,7 +57,7 @@
 						class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 						for="answer">Answer</label
 					><textarea
-						class="flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+						class="flex min-h-[80px] w-full rounded-md border border-input px-3 py-2 ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
 						id="answer"
 						name="back"
 						bind:value={$form.back}
