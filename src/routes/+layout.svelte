c75f0e852a84c7b47ef6be909176862743eea725
Sanyam Jain <xerosanyam@gmail.com>
2024-06-26 12:56:43 -0700

    fix layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script>
 	import PrimaryNav from '$lib/components/PrimaryNav.svelte';
 	import '../app.css';
+	import { dev, browser } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
+
+	import posthog from 'posthog-js';
+
+	if (browser) {
+		posthog.init('phc_9926SwyRC8yPRYf8le7laIwsnf1ygzhp3TtwXpYJ8Eq', {
+			api_host: 'https://us.i.posthog.com',
+			person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+		});
+	}
 	export let data;
 </script>
 
