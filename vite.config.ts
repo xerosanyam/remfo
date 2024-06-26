34474b59026edfa1e82ca5c4073f344b47288563
Sanyam Jain <xerosanyam@gmail.com>
2024-06-26 16:37:29 -0700

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import Icons from 'unplugin-icons/vite'
 import { enhancedImages } from '@sveltejs/enhanced-img';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
 	plugins: [
+		VitePWA({ registerType: 'autoUpdate' }),
 		enhancedImages(),
 		sveltekit(),
 		Icons({
