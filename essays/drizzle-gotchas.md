0399d4aa2c1854aeb3088c303dec7b6773587ff6
Sanyam Jain <xerosanyam@gmail.com>
2024-06-26 21:27:56 -0700

    add activity table

---
+++ essays/drizzle-gotchas.md
@@ -0,0 +1,5 @@
+## Drizzle
+
+### LibsqlError: SQL_NO_STATEMENT: SQL string does not contain any statemen
+
+- the last statement cannot be `--> statement-breakpoint`
--- src/lib/db/turso.schema.ts
+++ src/lib/db/turso.schema.ts
@@ -13,6 +13,9 @@ export const userTable = sqliteTable('auth_user', {
 	createdAt: integer('created_at', { mode: 'timestamp' }).default(sql`(unixepoch())`).notNull(),
 	updatedAt: integer('updated_at', { mode: 'timestamp' }).default(sql`(unixepoch())`).$onUpdate(() => new Date()).notNull(),
 });
+export type InsertUser = typeof userTable.$inferInsert;
+export type SelectUser = typeof userTable.$inferSelect;
+
 
 export const userSessionTable = sqliteTable('user_session', {
 	id: text('id').primaryKey(),
@@ -23,6 +26,9 @@ export const userSessionTable = sqliteTable('user_session', {
 	createdAt: integer('created_at', { mode: 'timestamp' }).default(sql`(unixepoch())`).notNull(),
 	updatedAt: integer('updated_at', { mode: 'timestamp' }).default(sql`(unixepoch())`).$onUpdate(() => new Date()).notNull(),
 });
+export type InsertSession = typeof userSessionTable.$inferInsert;
+export type SelectSession = typeof userSessionTable.$inferSelect;
+
 
 export const cardTable = sqliteTable('card', {
 	id: text('id').primaryKey(),
@@ -36,12 +42,15 @@ export const cardTable = sqliteTable('card', {
 	createdAt: integer('created_at', { mode: 'timestamp' }).default(sql`(unixepoch())`).notNull(),
 	updatedAt: integer('updated_at', { mode: 'timestamp' }).default(sql`(unixepoch())`).$onUpdate(() => new Date()).notNull(),
 })
-
-export type InsertUser = typeof userTable.$inferInsert;
-export type SelectUser = typeof userTable.$inferSelect;
-
-export type InsertSession = typeof userSessionTable.$inferInsert;
-export type SelectSession = typeof userSessionTable.$inferSelect;
-
 export type InsertCard = typeof cardTable.$inferInsert;
 export type SelectCard = typeof cardTable.$inferSelect;
+
+export const activityTable = sqliteTable('activity', {
+	id: text('id').primaryKey(),
+	action: text('action').notNull(),
+	cardId: text('card_id').notNull().references(() => cardTable.id),
+	userId: text('user_id').notNull().references(() => userTable.id),
+	createdAt: integer('created_at', { mode: 'timestamp' }).default(sql`(unixepoch())`).notNull(),
+});
+export type InsertActivity = typeof activityTable.$inferInsert;
+export type SelectActivity = typeof activityTable.$inferSelect;
