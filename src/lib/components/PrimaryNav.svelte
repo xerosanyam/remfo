11a361bf01dbcc0fe8c79ea78eee53255080206d
Sanyam Jain <xerosanyam@gmail.com>
2024-08-09 01:58:59 -0700

    improve nav

--- src/lib/components/PrimaryNav.svelte
+++ src/lib/components/PrimaryNav.svelte
@@ -41,7 +41,7 @@
 </script>
 
 <header
-	class="fixed bottom-0 z-10 flex w-full flex-col border-r bg-white shadow-lg sm:top-0 sm:h-screen sm:w-44"
+	class="fixed bottom-0 z-20 flex w-full flex-col border-r bg-white shadow-lg sm:top-0 sm:h-screen sm:w-44"
 >
 	<a class="hidden items-center p-2 text-gray-900 sm:flex md:mb-0" href="/">
 		<enhanced:img
@@ -61,11 +61,11 @@
 				</span>
 			</div>
 		</summary>
-		<nav class="flex w-screen sm:mt-8 sm:w-44 sm:flex-col sm:space-y-2">
+		<nav class="flex w-screen sm:mt-8 sm:w-44 sm:flex-col">
 			{#each links as link (link.href)}
 				<a
 					target={link.href.includes('https://') ? '_blank' : ''}
-					class={`${$page.url.pathname === link.href ? 'bg-gray-100' : ''} flex w-1/3 flex-col items-center whitespace-nowrap p-1 px-4 text-xs ring-offset-background transition-colors hover:bg-gray-200  hover:text-gray-900 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 sm:h-10 sm:w-full sm:flex-row sm:gap-2 sm:py-6 sm:text-base `}
+					class={`${$page.url.pathname === link.href ? 'bg-gray-100' : ''} border-r flex w-1/3 flex-col items-center whitespace-nowrap p-1 px-4 text-xs ring-offset-background transition-colors hover:bg-gray-200  hover:text-gray-900 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 sm:h-10 sm:w-full sm:flex-row sm:gap-2 sm:py-6 sm:text-base `}
 					href={link.href}
 				>
 					<svelte:component
