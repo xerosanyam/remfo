95d8cc679f682eea11a680d6c0683daa94b5fa34
Sanyam Jain <xerosanyam@gmail.com>
2024-08-19 22:48:27 -0700

    show streak

--- src/routes/(protected)/measure/Streak.svelte
+++ src/routes/(protected)/measure/Streak.svelte
@@ -3,11 +3,13 @@
 
 	export let data: info = [];
 	const maxStreakInfo = calculateMaxStreak(data);
+	const streak = calculateStreak(data, new Date());
+	// const compounded = `${(Math.pow(1.01, 365) * 100 - 100).toFixed(0)}%`;
 </script>
 
 <div class="rounded-sm border p-2 text-center text-gray-500">
 	<div class="text-lg">
-		Current Streak: <span class="text-gray-700">{calculateStreak(data, new Date())}</span>
+		Current Streak: <span class="cursor-default text-gray-700">{streak}</span>
 	</div>
 	{#if maxStreakInfo.maxStreak > 0}
 		<div class="text-xs">
