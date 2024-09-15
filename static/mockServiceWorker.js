365addb36dee287685f4b3741f70dbce0119efb1
Sanyam Jain <xerosanyam@gmail.com>
2024-09-14 20:46:11 -0700

    update packages

--- static/mockServiceWorker.js
+++ static/mockServiceWorker.js
@@ -123,7 +123,7 @@ async function handleRequest(event, requestId) {
   // Ensure MSW is active and ready to handle the message, otherwise
   // this message will pend indefinitely.
   if (client && activeClientIds.has(client.id)) {
-    ;(async function () {
+    ; (async function () {
       const responseClone = response.clone()
 
       sendToClient(
